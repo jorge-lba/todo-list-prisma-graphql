09716e71196c38be1585a2f961e10b427033f276
Jorge Alegretti <jorgeluiz.b.alegretti@gmail.com>
2021-09-04 20:30:10 -0300

    feat: implemented list todo use case

---
+++ src/useCases/listTodo/ListTodoUseCase.ts
@@ -0,0 +1,11 @@
+import { TodoDTO, TodoRepository } from "../../repositories/TodoRepository";
+
+class ListTodoUseCase {
+  constructor(private todoRepository: TodoRepository) {}
+
+  async execute(): Promise<TodoDTO[]> {
+    return this.todoRepository.findAll();
+  }
+}
+
+export { ListTodoUseCase };
