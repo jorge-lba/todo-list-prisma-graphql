4b7c5cc1a5c3a08e12acf8799f9e57b51f0486ea
Jorge Alegretti <jorgeluiz.b.alegretti@gmail.com>
2021-09-03 23:34:23 -0300

    feat: added return undifined in find by id

--- src/repositories/TodoRepository.ts
+++ src/repositories/TodoRepository.ts
@@ -8,7 +8,7 @@ interface TodoDTO {
 interface TodoRepository {
   create: ({ title, description }:TodoDTO) => Promise<TodoDTO>;
   findAll: () => Promise<TodoDTO[]>;
-  findById: (id:number) => Promise<TodoDTO>;
+  findById: (id:number) => Promise<TodoDTO | undefined>;
 }
 
 export { TodoDTO, TodoRepository };
