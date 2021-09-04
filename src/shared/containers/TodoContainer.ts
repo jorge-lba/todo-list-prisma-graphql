c0744f89d3f80f021cc4da8dd827bafae3b0cae0
Jorge Alegretti <jorgeluiz.b.alegretti@gmail.com>
2021-09-04 20:05:43 -0300

    feat: created containet to inject controllers

---
+++ src/shared/containers/TodoContainer.ts
@@ -0,0 +1,11 @@
+import { TodoRepository } from "../../repositories/TodoRepository"
+import { CreateTodoController, Controller } from "../../useCases/createTodo/CreateTodoController"
+import { CreateTodoUseCase } from "../../useCases/createTodo/CreateTodoUseCase"
+
+const createTodoController = (repository: TodoRepository) => {
+  const useCase = new CreateTodoUseCase(repository)
+
+  return new CreateTodoController(useCase)
+}
+
+export  { createTodoController }
