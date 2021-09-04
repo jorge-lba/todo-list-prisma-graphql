d62c3bd565abe58aeb06e0fc5fba433d9d42291e
Jorge Alegretti <jorgeluiz.b.alegretti@gmail.com>
2021-09-03 22:48:56 -0300

    feat: implemented create todo in memoty

---
+++ src/repositories/inMemory/TodoRepositoryImpInMemory.ts
@@ -0,0 +1,38 @@
+import { TodoDTO, TodoRepository } from "../TodoRepository";
+
+interface TodoCreateDTO {
+  title: string;
+  description: string;
+}
+
+class TodoRepositoryImpInMemory implements TodoRepository {
+  todos: Array<TodoDTO>
+  private static instance: TodoRepositoryImpInMemory
+
+  private constructor() {
+    this.todos = new Array<TodoDTO>();
+  }
+
+  static getInstance(): TodoRepositoryImpInMemory {
+    if(!this.instance){
+      this.instance = new TodoRepositoryImpInMemory();
+    }
+    return this.instance;
+  }
+
+  async create({
+    title,
+    description,
+  }: TodoCreateDTO): Promise<TodoDTO> {
+    const index = await this.todos.push({
+      id: this.todos.length + 1,
+      title,
+      description,
+      done: false
+    }) - 1;
+
+    return this.todos[index];
+  }
+}
+
+export { TodoRepositoryImpInMemory };
