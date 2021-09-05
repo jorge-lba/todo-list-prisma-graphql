004ca890d253b7453720a0ca9103408df25c2cc0
Jorge Alegretti <jorgeluiz.b.alegretti@gmail.com>
2021-09-04 21:16:51 -0300

    feat: implementede use case toggle done todo

---
+++ src/useCases/toggleDoneTodo/toggleDoneTodoUseCase.ts
@@ -0,0 +1,12 @@
+import { TodoDTO, TodoRepository } from "../../repositories/TodoRepository";
+
+class ToggleDoneTodoUseCase {
+  constructor(private todoRepository: TodoRepository) {}
+
+  async execute(todoId: number): Promise<TodoDTO | undefined> {
+    const todo = await this.todoRepository.toggleDoneById(todoId);
+    return todo;
+  }
+}
+
+export default ToggleDoneTodoUseCase;
