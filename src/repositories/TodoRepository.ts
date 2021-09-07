aa854d22efa4fa559a575728d0bbe2de004eaeec
Jorge Alegretti <jorgeluiz.b.alegretti@gmail.com>
2021-09-06 22:26:32 -0300

    refactor: some data has been changed to optional

--- src/repositories/TodoRepository.ts
+++ src/repositories/TodoRepository.ts
@@ -10,13 +10,18 @@ interface TodoCreateDTO {
   description: string;
 }
 
+interface UpdateTodoDTO {
+  title?: string;
+  description?: string;
+}
+
 interface TodoRepository {
   create: ({ title, description }:TodoCreateDTO) => Promise<TodoDTO>;
   findAll: () => Promise<TodoDTO[]>;
   findById: (id:number) => Promise<TodoDTO | undefined>;
   toggleDoneById: (id: number) => Promise<TodoDTO | undefined>;
-  update: (id: number, { title, description }: TodoDTO) => Promise<TodoDTO | undefined>;
+  update: (id: number, { title, description }: UpdateTodoDTO) => Promise<TodoDTO | undefined>;
   delete: (id: number) => Promise<boolean>;
 }
 
-export { TodoDTO, TodoRepository, TodoCreateDTO };
+export { TodoDTO, TodoRepository, TodoCreateDTO, UpdateTodoDTO };
