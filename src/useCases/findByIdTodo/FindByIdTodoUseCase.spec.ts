36d52f51afe81fc6496aa8514f9868fc26e2a92d
Jorge Alegretti <jorgeluiz.b.alegretti@gmail.com>
2021-09-08 01:16:24 -0300

    test: find by id use case

---
+++ src/useCases/findByIdTodo/FindByIdTodoUseCase.spec.ts
@@ -0,0 +1,37 @@
+import { TodoRepositoryImpInMemory } from "../../repositories/inMemory/TodoRepositoryImpInMemory";
+import { TodoDTO, TodoRepository } from "../../repositories/TodoRepository";
+import { FindByIdTodoUseCase } from "./FindByIdTodoUseCase";
+
+describe('Find By Id Use Case', () => {
+  let repository: TodoRepository;
+  let findByIdUseCase: FindByIdTodoUseCase;
+
+  let findByIdTodo: (id: number) => Promise<TodoDTO | undefined>;
+
+  beforeAll(() => {
+    repository = new TodoRepositoryImpInMemory();
+    findByIdUseCase = new FindByIdTodoUseCase(repository);
+
+    findByIdTodo = findById(findByIdUseCase);
+  });
+
+  it('should be return one todo', async () => {
+    const { id } = await repository.create({
+      title: 'Learn TypeScript',
+      description: 'Learn TypeScript with Node.js'
+    });
+
+    const todo = await findByIdTodo(id);
+
+    const expectedTitle = 'Learn TypeScript';
+    const expectedDescription = 'Learn TypeScript with Node.js';
+
+    expect(todo).toBeDefined();
+    expect(todo!.id).toBe(id);
+    expect(todo!.title).toBe(expectedTitle);
+    expect(todo!.description).toBe(expectedDescription);
+  })
+
+  const findById = (findByIdUseCase: FindByIdTodoUseCase) =>
+    async (id: number): Promise<TodoDTO | undefined> => await findByIdUseCase.execute(id);
+})
