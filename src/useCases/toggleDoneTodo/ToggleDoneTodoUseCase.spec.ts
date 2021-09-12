a5f8f7109b5119a52138cf99f72eea2db5263778
Jorge Alegretti <jorgeluiz.b.alegretti@gmail.com>
2021-09-11 22:42:19 -0300

    test: implemented toggle done todo

---
+++ src/useCases/toggleDoneTodo/ToggleDoneTodoUseCase.spec.ts
@@ -0,0 +1,32 @@
+import { TodoRepositoryImpInMemory } from "../../repositories/inMemory/TodoRepositoryImpInMemory";
+import { TodoDTO, TodoRepository } from "../../repositories/TodoRepository";
+import { ToggleDoneTodoUseCase } from "./toggleDoneTodoUseCase";
+
+describe('Toggle Done Todo Use Case', () => {
+  let repository: TodoRepository;
+  let toggleDoneUseCase: ToggleDoneTodoUseCase;
+
+  let toggleDoneTodo: (todoId: number) => Promise<TodoDTO | undefined>;
+
+  beforeAll(() => {
+    repository = new TodoRepositoryImpInMemory();
+    toggleDoneUseCase = new ToggleDoneTodoUseCase(repository);
+
+    toggleDoneTodo = toggleDone(toggleDoneUseCase);
+  });
+
+  it('should be changed state done the todo', async () => {
+    const { id } = await repository.create({
+      title: 'Todo 1',
+      description: 'Todo 1 description',
+    });
+
+    const todo = await toggleDoneTodo(id);
+
+    expect(todo!.done).toBeTruthy();
+    expect(todo!.done).toBe(true);
+  });
+
+  const toggleDone = (toggleDoneUseCase: ToggleDoneTodoUseCase) => 
+    async (todoId: number) => await toggleDoneUseCase.execute(todoId);
+});
