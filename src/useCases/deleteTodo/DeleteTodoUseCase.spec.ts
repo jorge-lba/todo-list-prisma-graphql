88687b11d147a5f18d04e38eabe9f5c99f793d70
Jorge Alegretti <jorgeluiz.b.alegretti@gmail.com>
2021-09-11 23:25:25 -0300

    test: implemented delete todo

---
+++ src/useCases/deleteTodo/DeleteTodoUseCase.spec.ts
@@ -0,0 +1,33 @@
+import { TodoRepositoryImpInMemory } from "../../repositories/inMemory/TodoRepositoryImpInMemory";
+import { TodoRepository } from "../../repositories/TodoRepository";
+import { DeleteTodoUseCase } from "./DeleteTodoUseCase";
+
+describe('Delete Todo Use Case', () => {
+  let repository: TodoRepository;
+  let deleteUseCase: DeleteTodoUseCase;
+
+  let deleteTodo: (todoId: number) => Promise<void>;
+
+  beforeAll(() => {
+    repository = new TodoRepositoryImpInMemory();
+    deleteUseCase = new DeleteTodoUseCase(repository);
+
+    deleteTodo = deleteOne(deleteUseCase);
+  });
+
+  it('should be delete one todo', async () => {
+    const  { id } = await repository.create({
+      title: 'todo 1',
+      description: 'description 1',
+    });
+
+    await deleteTodo(id);
+
+    const todoList = await repository.findAll();
+
+    expect(todoList.length).toBe(0);
+  });
+
+  const deleteOne = (deleteUseCase: DeleteTodoUseCase) => 
+    async (todoId: number) => await deleteUseCase.execute(todoId);
+});
