b56a4a8cae03a712919374acfe722983b9167196
Jorge Alegretti <jorgeluiz.b.alegretti@gmail.com>
2021-09-12 15:31:12 -0300

    refactor: move prisma files

--- src/shared/database/prisma/migrations/20210912033313_init/migration.sql
+++ src/shared/database/prisma/migrations/20210912033313_init/migration.sql
@@ -2,7 +2,7 @@
 CREATE TABLE "todo" (
     "id" SERIAL NOT NULL,
     "title" VARCHAR(255) NOT NULL,
-    "descrtiption" TEXT,
+    "description" TEXT,
     "done" BOOLEAN NOT NULL DEFAULT false,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
