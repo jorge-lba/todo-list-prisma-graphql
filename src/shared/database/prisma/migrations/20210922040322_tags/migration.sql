9c7c5006547826671b2a22cba3c8482c83e473d6
Jorge Alegretti <jorgeluiz.b.alegretti@gmail.com>
2021-09-22 01:04:36 -0300

    feat: create migration tags

---
+++ src/shared/database/prisma/migrations/20210922040322_tags/migration.sql
@@ -0,0 +1,10 @@
+-- CreateTable
+CREATE TABLE "tag" (
+    "id" SERIAL NOT NULL,
+    "name" VARCHAR(255) NOT NULL,
+    "description" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "tag_pkey" PRIMARY KEY ("id")
+);
