854e40a98c7be51a4bf758be676710433ec3372e
Jorge Alegretti <jorgeluiz.b.alegretti@gmail.com>
2021-09-22 01:14:00 -0300

    feat: implemented create tag

--- src/repositories/TagRepositoryImp.ts
+++ src/repositories/TagRepositoryImp.ts
@@ -1,12 +1,28 @@
+import { Prisma, PrismaClient, tag } from ".prisma/client";
+import { Database } from "../shared/database/prisma";
 import { CreateTagDTO, TagDTO, TagRepository } from "./TagRepository";
 
 class TagRepositoryImp implements TagRepository {
+  private model: Prisma.tagDelegate<tag>;
+
+  constructor(database: Database) {
+    // @ts-ignore
+    this.model = database.instance.prisma.tag;
+  }
+
   async create({ name, description }: CreateTagDTO): Promise<TagDTO>{
-    return {
-      id: 784,
-      name,
-      description
-    };
+    const tag = await this.model.create({
+      data: { name, description },
+      select: { 
+        id: true, 
+        name: true, 
+        description: true,
+        createdAt: true,
+        updatedAt: true
+      }
+    });
+    
+    return tag;
   };
 }
 
